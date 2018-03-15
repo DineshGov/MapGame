7bd958b9ee3ccd88630e7d40488415b7555e8987
DineshGov <gov.dinesh@gmail.com>
2018-03-15 12:41:33 +0100

    ajout de js et partitioning

--- inscription.php
+++ inscription.php
@@ -1,57 +1,40 @@
-<!DOCTYPE html>
-<html lang="fr">
-  <head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta name="description" content="">
-    <meta name="author" content="">
+<?php 
+$page_name = "inscription.php";
+require('header.php');
+require('database_auth.php'); ?>
 
-    <link rel="icon" href="bootstrap/favicon_Se7en.ico">
-    <title>Se7en</title>
+<script type="text/javascript"> 
 
-    <script src="js/jquery-3.3.1.js"></script>
-    <link href="bootstrap/css/bootstrap.min.css" rel="stylesheet">
-    <link href="bootstrap/css/ie10-viewport-bug-workaround.css" rel="stylesheet">
-    <link href="authentification.css" rel="stylesheet">
-    <script src="bootstrap/js/ie-emulation-modes-warning.js"></script>
-  </head>
+  var msg_error_created = false;
+  function egalite_mdp(){
+    if ($('#inputPasswordConfirm').val() !== $('#inputPassword').val()){
+        if($('#inputPassword').val() !== '' && $('#inputPasswordConfirm').val() !== ''){
+          if(!msg_error_created){
+            var msg_error = '<p id="msg_error" class="h6 text-danger">Mots de passe différents</p>';
+            $('.auth_form_submitter').removeClass('active');
+            $('.auth_form_submitter').addClass('disabled');
+            $('.auth_form_submitter').css('pointer-events', 'none');
+            $('#form_inscription').append(msg_error);
+          }
+          msg_error_created = true;
+        }
+      }
+    else{
+      $('#msg_error').remove();
+      $('.auth_form_submitter').removeClass('disabled');
+      $('.auth_form_submitter').addClass('active');
+      $('.auth_form_submitter').css('pointer-events', 'auto');
+      if (msg_error_created)
+        msg_error_created = false;
+    }
+  }
 
-  <body>
+</script>
 
-    <nav class="navbar navbar-inverse navbar-static-top">
-      <div class="container">
-        <div class="navbar-header">
-          <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
-            <span class="sr-only">Toggle navigation</span>
-            <span class="icon-bar"></span>
-            <span class="icon-bar"></span>
-            <span class="icon-bar"></span>
-          </button>
-          <a class="navbar-brand" href="#" id="game_name_link">Se7en</a>
-        </div>
-
-        <div id="navbar" class="navbar-collapse collapse">
-          <ul class="nav navbar-nav">
-            <li class="active"><a id="about_link">About</a></li>
-            <script type="text/javascript"> 
-              $('#about_link').on('click', function(){
-                alert('Informations sur les développeurs blabla');
-              })
-            </script>
-          </ul>
-
-          <ul class="nav navbar-nav navbar-right">
-            <li><a href="connexion.php"><span class="glyphicon glyphicon-log-in"></span> Connexion</a></li>
-            <li><a href="#"><span class="glyphicon glyphicon-user "></span> Invité</a></li>
-          </ul>
-        </div>
-      </div>
-    </nav>
 
     <div class="container">
 
-      <form class="form-signin">
+      <form class="form-signin" id="form_inscription">
         <h2 class="form-signin-heading">Inscription</h2>
         
         <div class="input-group">
@@ -62,14 +45,16 @@
         <div class="input-group">
           <span class="input-group-addon"><i class="glyphicon glyphicon-lock"></i></span>
           <input type="password" id="inputPassword" class="form-control" placeholder="Mot de passe">
+          <script type="text/javascript"> $('#inputPassword').on('input', egalite_mdp); </script>
         </div>
 
         <div class="input-group" id="passwordConfirmContainer">
           <span class="input-group-addon"><i class="glyphicon glyphicon-hand-up"></i></span>
           <input type="password" id="inputPasswordConfirm" class="form-control" placeholder="Confirmez mot de passe">
+          <script type="text/javascript"> $('#inputPasswordConfirm').on('input', egalite_mdp); </script>
         </div>
 
-        <button class="btn btn-lg btn-danger btn-block" type="submit" id="auth_form_submitter">Inscription</button>
+        <button class="btn btn-lg btn-danger btn-block auth_form_submitter active" type="submit">Inscription</button>
       </form>
 
     </div>
