997f1961c688620c575a093d1c80e7fbc825d053
DineshGov <gov.dinesh@gmail.com>
2018-03-17 23:36:24 +0100

    commit avant pull

--- connexion.php
+++ connexion.php
@@ -5,17 +5,17 @@ require('database_auth.php');?>
 
     <div class="container">
 
-      <form class="form-signin" action="test.php" method="post">
+      <form class="form-signin" action="redirection_connexion.php" method="post">
         <h2 class="form-signin-heading">Connexion</h2>
         
         <div class="input-group">
           <span class="input-group-addon"><i class="glyphicon glyphicon-user"></i></span>
-          <input type="text" name="inputLogin" id="inputLogin" class="form-control" placeholder="Nom d'utilisateur" maxlength="20" minlength="4" required autofocus>
+          <input type="text" name="inputLogin" id="inputLogin" class="form-control" placeholder="Nom d'utilisateur" maxlength="50" minlength="6" required autofocus>
         </div>
 
         <div class="input-group">
           <span class="input-group-addon"><i class="glyphicon glyphicon-lock"></i></span>
-          <input type="password" name="inputPassword" id="inputPassword" maxlength="100" minlength="6" class="form-control" placeholder="Mot de passe">
+          <input type="password" name="inputPassword" id="inputPassword" maxlength="100" minlength="6" class="form-control" placeholder="Mot de passe" required>
         </div>
 
         <button class="btn btn-lg btn-danger btn-block auth_form_submitter" id="buttonConnexion" type="submit">Connexion</button>
