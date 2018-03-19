cf89caf40bf1b0f4cb0a0221e0006e24f202bbca
DineshGov <gov.dinesh@gmail.com>
2018-03-19 21:55:17 +0100

    commit avant push

--- redirection_connexion.php
+++ redirection_connexion.php
@@ -2,3 +2,13 @@
 $page_name = "redirection_connexion.php"; 
 require('header.php');
 require('database_auth.php');?>
+
+<?php 
+echo "<pre>";
+print_r($_POST);
+echo "</pre>";
+?>
+
+<div class="div_redirection">
+
+</div>
