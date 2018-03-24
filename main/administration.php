677f21be40e5e2c7ace5341ded7829e033bde0fa
DineshGov <gov.dinesh@gmail.com>
2018-03-24 01:24:43 +0100

    page administrateur avancée

--- main/administration.php
+++ main/administration.php
@@ -3,21 +3,40 @@
     require('../database_auth.php');
 ?>
 
+<script type="text/javascript">
+  $('html').on('click',function(){
+    console.log("test");
+  });
+</script>
 
+    <div class="col-lg-12 col-md-12 col-sm-12">
+      <div class="col-lg-offset-1 col-lg-11 col-md-offset-1 col-md-11 col-sm-offset-1 col-sm-11"> <h1>Administration</h1> </div>
+      <div class="col-lg-offset-2 col-lg-10 col-md-offset-2 col-md-10 col-sm-offset-2 col-sm-10"> 
+        <h3 id="chevron-bas">Gestion utilisateur <span class="glyphicon glyphicon-chevron-down"></span></h3>
+      </div>
 
-    <div>
-      <div class="col-lg-offset-1 col-md-offset-1 col-sm-offset-1"> <h2>Bienvenue !</h2> </div>
-
-      <?php
-        $req=$bd->prepare('select * from Users');
-        $req->execute();
-
-        echo '<div class="col-lg-offset-3 col-lg-6 col-lg-offset-3 col-md-offset-3 col-md-6 col-md-offset-3 col-sm-offset-3 col-sm-6 col-sm-offset-3" style="text-align: center;">';
-        while($tab = $req->fetch(PDO::FETCH_ASSOC)){
-          echo "<button class='btn btn-lg btn-danger btn-block auth_form_submitter' id='buttonConnexion' type='submit'>" . htmlspecialchars($tab['login'], ENT_QUOTES) . "</button>";
-        }
-        echo "</div>";
-
-      ?>
-
+      <div class="col-lg-offset-3 col-lg-6 col-lg-offset-3 col-md-offset-3 col-md-6 col-md-offset-3 col-sm-offset-3 col-sm-6 col-sm-offset-3">
+        <table style="background-color: #ddd;" class="table table-striped table-bordered">
+          <thead>
+            <tr style="background-color: #aaa; font-weight: bold;">
+              <th>id</th>
+              <th>Nom d'utilisateur</th>
+              <th>Date d'inscription</th>
+            </tr>
+          </thead>
+          <tbody>
+            <?php
+              $req=$bd->prepare('select * from Users');
+              $req->execute();
+                    while($tab = $req->fetch(PDO::FETCH_ASSOC)){
+                      echo '<tr>';
+                      echo "<td>" . $tab['id'] . "</td>";
+                      echo "<td>" . htmlspecialchars($tab['login'], ENT_QUOTES) . "</td>";
+                      echo "<td>" . $tab['date_inscription'] . "</td>";
+                      echo '</tr>';
+                    }
+            ?>
+          </tbody>
+        </table>
+      </div>
     </div>
--- main/menu_principal.php
+++ main/menu_principal.php
@@ -5,20 +5,22 @@
 
 
 
-      <div class="col-lg-offset-1 col-md-offset-1 col-sm-offset-1 col-lg-11 col-md-11 col-sm-11"> <h2>Bienvenue !</h2> </div>
+    <div class="col-lg-offset-1 col-md-offset-1 col-sm-offset-1 col-lg-11 col-md-11 col-sm-11"> <h2>Bienvenue !</h2> </div>
 
-      <?php
-        $req=$bd->prepare('select * from Questionnaires');
-        $req->execute();
+    <?php
+      $req=$bd->prepare('select * from Questionnaires');
+      $req->execute();
 
-        echo '<div class="col-lg-offset-3 col-lg-6 col-lg-offset-3 col-md-offset-3 col-md-6 col-md-offset-3 col-sm-offset-3 col-sm-6 col-sm-offset-3" style="text-align: center;">';
-        while($tab = $req->fetch(PDO::FETCH_ASSOC)){
-          echo "<button class='btn btn-lg btn-danger btn-block auth_form_submitter' id='buttonConnexion' type='submit'>" . $tab['nomQuestionnaire'] . "</button>";
-        }
-        echo "</div>";
+      echo '<div class="col-lg-offset-3 col-lg-6 col-lg-offset-3 col-md-offset-3 col-md-6 col-md-offset-3 col-sm-offset-3 col-sm-6 col-sm-offset-3" style="text-align: center;">';
+      while($tab = $req->fetch(PDO::FETCH_ASSOC)){
+        echo "<button class='btn btn-lg btn-danger btn-block auth_form_submitter' id='buttonConnexion' type='submit'>" . $tab['nomQuestionnaire'] . "</button>";
+      }
+      echo "</div>";
 
-      ?>
     
-    <div class="col-lg-offset-1 col-lg-11 col-md-offset-1 col-md-11 col-sm-offset-1 col-sm-11">
-      <a href="administration.php"><h2><span class="glyphicon glyphicon-cog"></span>Panneau de configuration</h2></a> 
-    </div>
+    if(isset($_SESSION['statut']) && $_SESSION['statut']==="admin")
+      echo '<div class="col-lg-offset-1 col-lg-11 col-md-offset-1 col-md-11 col-sm-offset-1 col-sm-11">
+        <a href="administration.php"><h2><span class="glyphicon glyphicon-cog"></span>Panneau de configuration</h2></a> 
+      </div>'
+
+    ?>
