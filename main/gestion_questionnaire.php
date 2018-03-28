bfaa912ba5909036e2f880d8015af48f53556931
DineshGov <gov.dinesh@gmail.com>
2018-03-28 16:50:42 +0200

    recuperation des coordonnées fonctionnelle

--- main/gestion_questionnaire.php
+++ main/gestion_questionnaire.php
@@ -15,4 +15,75 @@
     	echo "</pre>";
     ?>
 
+    <div class="col-lg-12 col-md-12 col-sm-12" style="background-color: red;">
+    </div>
+
+	<div class="col-lg-offset-1 col-lg-10 col-lg-offset-1 col-md-offset-1 col-md-10 col-md-offset-1 col-sm-offset-1 col-sm-10 col-sm-offset-1" id="mapContainer" style="height: 460px; margin-top: 20px; margin-bottom: 20px;">   
+    </div>
+      
+      
+    <div class="col-lg-10 col-md-10 col-sm-10" style="margin: 10px;">
+
+        <p>Latitude: <input type="text" id="clickedLatitude"> 
+        Longitude: <input type="text" id="clickedLongitude"></p>
+        <p>Attribuer ces coordonnées à la question 
+            <select id="questionSelect">
+                <option value="default" selected></option> 
+                <option value="valeur1">1</option> 
+                <option value="valeur2">2</option>
+                <option value="valeur3">3</option>
+                <option value="valeur4">4</option> 
+                <option value="valeur5">5</option>
+                <option value="valeur6">6</option>
+                <option value="valeur7">7</option> 
+        </p>
+
+    </div>
+
+    <script type="text/javascript">
+        
+        map = L.map('mapContainer').setView([48.858376, 2.294442], 3);
+        //La map est créée mais elle ne possède pas encore de tuiles.
+
+        L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', 
+        {
+        attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
+        maxZoom: 13,
+        id: 'mapbox.streets',
+        accessToken: 'pk.eyJ1IjoiZGppbiIsImEiOiJjamZheWg1OWoxaHYzM3VtejB5OWZxcXVwIn0.i_GA2vSOytwgViYnIvlGGA'
+        }).addTo(map);
+
+        function onMapClick(e) {
+        /*Exemple de valeur retournée par e.latlng.toString()
+        LatLng(45.58329, 2.640128)
+
+        On veut extraire les coordonnées pour avoir deux variables latitude et longitude.
+        */
+        var marker = L.marker(e.latlng).addTo(map);
+
+        var coordonnees = e.latlng.toString();
+        var position_parenthese_ouvrante = coordonnees.indexOf('(') + 1;
+        var position_parenthese_fermante = coordonnees.indexOf(')');
+
+        console.log("coordonnees = " + coordonnees);
+        console.log("position_parenthese_ouvrante = " + position_parenthese_ouvrante);
+        console.log("position_parenthese_fermante = " + position_parenthese_fermante);
+
+        var coordonnees_sans_parenthese = coordonnees.substring(position_parenthese_ouvrante, position_parenthese_fermante);
+        
+        console.log("coordonnees_sans_parenthese = " + coordonnees_sans_parenthese);
+
+        var extractionCoordonnees = coordonnees_sans_parenthese.split(", ");
+
+        console.log("latitude= " + extractionCoordonnees[0]);
+        console.log("longitude= " + extractionCoordonnees[1]);
+
+        $("#clickedLatitude").val(extractionCoordonnees[0]);
+        $("#clickedLongitude").val(extractionCoordonnees[1]);
+        }
+
+        map.on('click', onMapClick);
+
+    </script>
+
   </div>
--- main/header.php
+++ main/header.php
@@ -8,7 +8,13 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
     <link rel="icon" href="../bootstrap/favicon_Se7en.ico">
-    
+    <?php
+    if($page_name==="gestion_questionnaire.php"){
+      echo '<link rel="stylesheet" href="https://unpkg.com/leaflet@1.3.1/dist/leaflet.css" integrity="sha512-Rksm5RenBEKSKFjgI3a41vrjkw4EVPlJ3+OiI65vTjIdo9brlAacEuKOiQ5OFh7cOI1bkDwLqdLw3Zg0cRJAAQ==" crossorigin=""/>';
+      echo '<script src="https://unpkg.com/leaflet@1.3.1/dist/leaflet.js" integrity="sha512-/Nsx9X4HebavoBvEBuyp3I7od5tA0UzAxs+j83KgC8PU0kgB4XiK4Lfe4y4cgBtaRJQEIFCW+oC506aPT2L1zw==" crossorigin=""></script>';
+    }
+    ?>    
+
     <link href="../bootstrap/css/bootstrap.min.css" rel="stylesheet">
     <script src="../jquery/jquery-3.3.1.js"></script>
     <script src="../bootstrap/js/bootstrap.min.js"></script>
