d2190fa65fdf0b7f40d0ed3894cceaa2aacb9b69
DineshGov <gov.dinesh@gmail.com>
2018-05-04 17:43:16 +0200

    page creation_questionnaire en cours

--- main/creation_questionnaire.php
+++ main/creation_questionnaire.php
@@ -9,4 +9,6 @@
 	<div class="col-lg-offset-1 col-lg-11 col-md-offset-1 col-md-11 col-sm-offset-1 col-sm-11"> <h1>Création questionnaire</h1>
 	</div>
 
+	//aller à la page gestion_questionnaire si questionnaire crée.
+
 </div>
