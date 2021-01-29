06802be6b735c0be1ee0d5c648f71d62ddee1e2c
assan <assanivory@gmail.com>
2021-01-29 14:33:10 +0100

    mapgame

--- main/js/correction.js
+++ main/js/correction.js
@@ -151,13 +151,14 @@ function click_on_btn_left(question_affichee){
 function init(){
 	map = L.map('carte').setView([48.858376, 2.294442], 3);
 
-    L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', 
-    {
-    attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
-    maxZoom: 9,
-    id: 'mapbox.streets',
-    accessToken: 'pk.eyJ1IjoiZGppbiIsImEiOiJjamZheWg1OWoxaHYzM3VtejB5OWZxcXVwIn0.i_GA2vSOytwgViYnIvlGGA'
-    }).addTo(map);
+	L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoiZW5pZ21hNzc3NyIsImEiOiJja2szNnZ0dnkwcWZ4MnBxbzFvbXJhOXZzIn0.TmXyfODNPIi0hmAJYru9Tw', {
+        attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+		maxZoom: 18,
+		id: 'mapbox/streets-v11',
+		tileSize: 512,
+		zoomOffset: -1,
+		accessToken: 'pk.eyJ1IjoiZW5pZ21hNzc3NyIsImEiOiJja2szNnZ0dnkwcWZ4MnBxbzFvbXJhOXZzIn0.TmXyfODNPIi0hmAJYru9Tw'
+      }).addTo(map);
 
     select_all_from_question();
 
